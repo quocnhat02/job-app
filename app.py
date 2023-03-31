87441bd87f30a50104bec3a79afbfc06a285782d
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-03-31 23:23:19 +0700

    exercise

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-# number = 100
-# while number > 0:
-#     print(number)
-#     number //= 2
-
-command = ""
-while command.lower() != "quit":
-    command = input(">")
-    print("ECHO", command)
+count = 0
+for number in range(1, 10):
+    if number % 2 == 0:
+        count += 1
+        print(number)
+# for number in range(2, 10, 2):
+#         count += 1
+#         print(number)
+print(f"We have {count} even numbers")
