8d1349c5c9e0026861e53de219ce6bf2b7f97938
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-03-31 21:25:34 +0700

    escape sequences

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-course = 'python'
+course = 'python\'python'
 
-print(len(course))
-
-print(course[0])
-print(course[-1])
-print(course[0:3])
-print(course[0:])
-print(course[:3])
-print(course[:])
+print(course)
