02a458d605c2b674b6977c5c4fdd6d9aa8cdaeb7
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-03-31 23:14:59 +0700

    while loops

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-# Iterable
-for x in [1, 2, 3, 4]:
-    print(x)
+# number = 100
+# while number > 0:
+#     print(number)
+#     number //= 2
+
+command = ""
+while command.lower() != "quit":
+    command = input(">")
+    print("ECHO", command)
