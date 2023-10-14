2aa37a0e701265bfb8db62b0739649b39c83ad29
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-10-14 16:37:04 +0700

    math

--- app.py
+++ app.py
@@ -1,19 +1,6 @@
-# List
-mixed = [1, 2, 3, 4, 5, "Hello", [1, 2, 3]]
-print(mixed)
-print(type(mixed))
+# len()
+address = "VietNam"
+name = "Nhat"
 
-# Dictionary
-user_info = {"user_name": "Light", "user_id": 1}
-print(user_info)
-print(type(user_info))
-
-# Tuple
-mixed_tuple = (1, 2, 3, 4, 5, "Hello", [1, 2, 3])
-print(mixed_tuple)
-print(type(mixed_tuple))
-
-# Set
-mixed_set = {1, 2, 3.3, 5.3, "Python", "Abc", 101}
-print(mixed_set)
-print(type(mixed_set))
+full = f"{name} in {address}"
+print(round(2.6))
