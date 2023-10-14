1cc04079e3c01ecf8b2d10a43cf70be5ed599fc2
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-10-14 12:34:03 +0700

    data types 2

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-# Integer
-age = 21
-print(age)
-print(type(age))
+# List
+mixed = [1, 2, 3, 4, 5, "Hello", [1, 2, 3]]
+print(mixed)
+print(type(mixed))
 
-# Float
-grade = 8.5
-print(grade)
-print(type(grade))
+# Dictionary
+user_info = {"user_name": "Light", "user_id": 1}
+print(user_info)
+print(type(user_info))
 
-# Boolean
-online = True
-offline = False
-print(online, offline)
+# Tuple
+mixed_tuple = (1, 2, 3, 4, 5, "Hello", [1, 2, 3])
+print(mixed_tuple)
+print(type(mixed_tuple))
+
+# Set
+mixed_set = {1, 2, 3, 4, 5, 3.3, "Hello", "Abc"}
+print(mixed_set)
+print(type(mixed_set))
