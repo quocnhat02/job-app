67e24bd64d8b7c87200c79d707c78c29738ba54c
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-10-14 17:20:00 +0700

    for 2

--- app.py
+++ app.py
@@ -8,5 +8,10 @@
 # for a in range(5,15):
 #     print(a)
 
-for a in range(10, 100, 10):
-    print(a)
+# for a in range(0, 100, 10):
+#     print(a)
+
+numbers = [1, 2, 3, 4, 5]
+
+for index, number in enumerate(numbers):
+    print(index, number)
